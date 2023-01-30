4d8ef2f1350f10f03da2370d88b5e498965f3208
BarakaIsaac <isaac.mwandau2@gmail.com>
2023-01-30 23:31:01 +0300

    Completed Assignments

--- src/components/App.js
+++ src/components/App.js
@@ -10,11 +10,11 @@ console.log(user);
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+        <NavBar/>
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links} linkedin={user.linkedin} />
     </div>
-  );
+    );
 }
 
 export default App;
